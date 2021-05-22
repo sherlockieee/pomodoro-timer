0111eab51bf6ff5841ca595b2c74ffb712f4edf5
Sherlockieee <phuonghatrannguyen00@gmail.com>
2021-05-22 23:43:22 +0700

    update play pause timer

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ export default class App extends Component {
       workTime: 25,
       breakTime: 5,
       display: "25:00",
-      current: "work"
+      current: "work",
+      interval: null,
     }
 
     this.togglePlayButton = this.togglePlayButton.bind(this);
@@ -30,6 +31,7 @@ export default class App extends Component {
     this.decrease = this.decrease.bind(this);
     this.reset = this.reset.bind(this);
     this.handleStartStop = this.handleStartStop.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
 
   togglePlayButton(){
@@ -43,12 +45,23 @@ export default class App extends Component {
     if (!playing){
       if (type === 'work' && workTime < 60){
         this.setState({
-          workTime: workTime + 1
+          workTime: workTime + 1,
         })
+        if (this.state.current == 'work'){
+          this.setState({
+            display: workTime + 1 >= 10? String(workTime + 1) + ":00" : "0" + String(workTime + 1) + ":00"
+          })
+        }
       } else if (type === 'break' && breakTime < 60){
         this.setState({
-          breakTime: breakTime + 1
+          breakTime: breakTime + 1 ,
         })
+
+        if (this.state.current == 'break'){
+          this.setState({
+            display: breakTime + 1 >= 10? String(breakTime + 1) + ":00" : "0" + String(breakTime + 1) + ":00"
+          })
+        }
       }
     }
   }
@@ -60,25 +73,82 @@ export default class App extends Component {
         this.setState({
           workTime: workTime - 1
         })
+        if (this.state.current == 'work'){
+          this.setState({
+            display: workTime - 1 >= 10? String(workTime - 1) + ":00" : "0" + String(workTime - 1) + ":00"
+          })
+        }
       } else if (type === 'break' && breakTime > 1) {
         this.setState({
           breakTime: breakTime - 1
         })
+
+        if (this.state.current == 'break'){
+          this.setState({
+            display: breakTime - 1 >= 10? String(breakTime - 1) + ":00" : "0" + String(breakTime - 1) + ":00"
+          })
+        }
       }
     }
     
   }
 
   reset(){
+    this.stopTimer();
     this.setState({
       playing: false,
       workTime: 25,
       breakTime: 5,
+      display: "25:00",
+      current: "work",
+      interval: null
     })
   }
 
   handleStartStop(){
     this.togglePlayButton();
+    if (!this.state.playing){
+      if (this.state.current == "work"){
+        this.startTimer(this.state.workTime, "work")
+      } else {
+        this.startTimer(this.state.breakTime, "break")
+      }
+    } else {
+      this.stopTimer()
+    }
+  }
+
+  startTimer(time, type){
+    let minutes = time - 1
+    let seconds = 59
+    
+    let intervalID = setInterval(() => {
+      let minuteStr = String(minutes).length == 1 ? "0" + String(minutes) : String(minutes);
+      let secondStr = String(seconds).length == 1 ? "0" + String(seconds) : String(seconds);
+      this.setState({
+        display: minuteStr + ":" + secondStr
+      })
+
+      if (seconds > 0){
+        seconds -= 1
+      } else if (minutes > 0){
+        minutes -= 1
+        seconds = 59
+      } else {
+        this.stopTimer();
+        type == 'work'? this.startTimer(this.state.breakTime, 'break') : this.startTimer(this.state.breakTime, 'work') 
+      }
+    }, 1000)
+
+    this.setState({
+      interval: intervalID,
+    }
+    )
+    
+  }
+
+  stopTimer = () => {
+    clearInterval(this.state.interval);
   }
   
   render() {
@@ -90,7 +160,7 @@ export default class App extends Component {
 
         <WorkSession increase = {this.increase} decrease = {this.decrease} display = {this.state.workTime}/>
 
-        <CountdownDisplay display = {this.state.display}/>
+        <CountdownDisplay display = {this.state.display} current = {this.state.current}/>
 
         <button id='start_stop' onClick = {this.handleStartStop}>{this.state.playing? "Pause" : "Start"}</button>
         <button id = 'reset' onClick = {this.reset}> Reset </button>
--- src/components/countdownDisplay.js
+++ src/components/countdownDisplay.js
@@ -4,7 +4,7 @@ export default class countdownDisplay extends Component {
     render() {
         return (
             <div>
-                <p id = 'timer-label'> Session </p>
+                <p id = 'timer-label'> {this.props.current == 'work'? 'Work Session': 'Break Session'} </p>
                 <h2 id = 'time-left'>{this.props.display}</h2>
             </div>
         )
